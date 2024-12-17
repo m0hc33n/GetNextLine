8a615bbdc83efa3c7c65e094c064cd1ca97af683
r3d4g3n7 <123310235+m0hc33n@users.noreply.github.com>
2024-12-17 16:53:13 +0100

    Add files via upload
    BONUS_TIME

---
+++ get_next_line_bonus.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamazian <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/17 01:17:47 by mamazian          #+#    #+#             */
+/*   Updated: 2024/11/18 04:08:21 by mamazian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdbool.h>
+# include <stdint.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define FD_MAX 256
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# define NEW_LINE '\n'
+
+typedef struct s_tracker
+{
+	char				*buffer;
+	struct s_tracker	*next;
+}						t_tracker;
+
+// UTILS
+bool		check_line(t_tracker *tracker);
+t_tracker	*train_tail(t_tracker *tracker);
+uint32_t	get_line_size(t_tracker *tracker);
+void		unite_wagon(t_tracker *tracker, char *wagon);
+void		free_wagons(t_tracker **tracker, t_tracker *fish_wagon);
+// CORE
+char		*get_next_line(int fd);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamazian <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/17 01:31:28 by mamazian          #+#    #+#             */
+/*   Updated: 2024/11/18 04:08:59 by mamazian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+bool	check_line(t_tracker *tracker)
+{
+	t_tracker	*tail;
+	uint32_t	i;
+
+	if (!tracker)
+		return (false);
+	i = 0;
+	tail = train_tail(tracker);
+	while (tail->buffer[i])
+	{
+		if (tail->buffer[i] == NEW_LINE)
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+t_tracker	*train_tail(t_tracker *head)
+{
+	if (!head)
+		return (NULL);
+	while (head->next)
+		head = head->next;
+	return (head);
+}
+
+uint32_t	get_line_size(t_tracker *tracker)
+{
+	uint32_t	sz;
+	uint32_t	i;
+
+	if (!tracker)
+		return (0);
+	sz = 0;
+	while (tracker)
+	{
+		i = 0;
+		while (tracker->buffer[i])
+		{
+			if (tracker->buffer[i] == NEW_LINE)
+				return (++sz);
+			sz++;
+			i++;
+		}
+		tracker = tracker->next;
+	}
+	return (sz);
+}
+
+void	unite_wagon(t_tracker *tracker, char *wagon)
+{
+	uint32_t	i;
+
+	if (!tracker)
+		return ;
+	while (tracker)
+	{
+		i = 0;
+		while (tracker->buffer[i])
+		{
+			if (tracker->buffer[i] == NEW_LINE)
+			{
+				*wagon++ = NEW_LINE;
+				*wagon = 0;
+				return ;
+			}
+			*wagon++ = tracker->buffer[i++];
+		}
+		tracker = tracker->next;
+	}
+	*wagon = 0;
+}
+
+void	free_wagons(t_tracker **tracker, t_tracker *fish_wagon)
+{
+	t_tracker	*tmp;
+
+	if (!*tracker)
+		return ;
+	while (*tracker)
+	{
+		tmp = (*tracker)->next;
+		free((*tracker)->buffer);
+		free(*tracker);
+		*tracker = tmp;
+	}
+	*tracker = NULL;
+	if (fish_wagon && fish_wagon->buffer[0])
+		*tracker = fish_wagon;
+	else if (fish_wagon)
+	{
+		free(fish_wagon->buffer);
+		free(fish_wagon);
+	}
+}
